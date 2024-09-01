df7c0442e0a73a554ca14617019dd77f79e593fb
chetanjangra12 <chetanjangragju@gmail.com>
2024-09-01 14:26:06 +0530

    final commit1

--- lib/story.dart
+++ lib/story.dart
@@ -1,5 +1,7 @@
 
-class Story{
-  final String storyTitle,choice1,choice2;
-  Story({required this.storyTitle,required this.choice1,required this.choice2} );
+class Story {
+  final String storyTitle, choice1, choice2;
 
+  Story(
+      {required this.storyTitle, required this.choice1, required this.choice2});
+}
